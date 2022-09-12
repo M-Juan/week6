4fc800fc201379cd8d88df301dffc7bc645aa1ce
amy93514 <amy93514@yahoo.com.tw>
2022-09-12 22:33:00 +0800

    Update 2022-09-12T14:32:43.275Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,62 +1,45 @@
-$(function() {
+"use strict";
+
+$(function () {
   console.log('Hello Bootstrap5');
 });
-
-
-
-
-
 var swiper1 = new Swiper(".course-swiper", {
   autoplay: {
-      disableOnInteraction: false,
-      delay: 0
+    disableOnInteraction: false,
+    delay: 0
   },
   speed: 1500,
   loop: true,
   spaceBetween: 16,
   slidesPerView: "3",
   breakpoints: {
-    375:{
-      slidesPerView: 3,
+    375: {
+      slidesPerView: 3
     },
-
     768: {
-        slidesPerView: 6
-      },
+      slidesPerView: 6
+    },
     992: {
-        slidesPerView: 9
-      }
+      slidesPerView: 9
+    }
   }
-
 });
-
-
-
-
-
-const swiper = new Swiper('.swiper', {
+var swiper = new Swiper('.swiper', {
   // Optional parameters
   effect: 'fade',
   fadeEffect: {
     crossFade: true
   },
-  
   // If we need pagination
   pagination: {
-    el: '.swiper-pagination',
+    el: '.swiper-pagination'
   },
-
   // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+    prevEl: '.swiper-button-prev'
+  }
 });
-
-
-
-
-
 var swiper2 = new Swiper(".myswiper", {
   // centeredSlides: true,
   // autoplay: {
@@ -73,13 +56,11 @@ var swiper2 = new Swiper(".myswiper", {
       slidesPerView: 3
     },
     768: {
-          slidesPerView: 6
+      slidesPerView: 6
     },
     992: {
-          slidesPerView: 6
-   }
+      slidesPerView: 6
+    }
   }
-
 });
-
-
+//# sourceMappingURL=all.js.map
